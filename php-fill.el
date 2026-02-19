e68bd8b1d27c7f435d1922bf80aea59410125376
Ariel Del Valle Lozano <arielmazatlan@gmail.com>
2026-02-19 11:48:08 -0700

    v1.1.0

--- php-fill.el
+++ php-fill.el
@@ -5,7 +5,7 @@
 ;; Author: Ariel Del Valle Lozano <arielmazatlan@gmail.com>
 ;; Maintainer: Ariel Del Valle Lozano <arielmazatlan@gmail.com>
 ;; URL: https://github.com/arielenter/php-fill.el
-;; Version: 1.0.0
+;; Version: 1.1.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: php, languages, tools, convenience
 
